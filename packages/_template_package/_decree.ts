989e897aa44c360fc0bcac35453af79dfa0e08b1
Artur Kurowski <radarsu@gmail.com>
2021-08-26 10:38:32 +0200

    chore(core): update

--- packages/_template_package/_decree.ts
+++ packages/_template_package/_decree.ts
@@ -1,15 +1,17 @@
 import type * as Enquirer from 'enquirer';
 
 const getDecree = async (enquirer: Enquirer) => {
-    const responses = await enquirer.prompt([
-        {
-            message: `Name:`,
-            name: `name`,
-            type: `input`,
-        },
-    ]);
+    // const responses = await enquirer.prompt([
+    //     {
+    //         message: `Name:`,
+    //         name: `name`,
+    //         type: `input`,
+    //     },
+    // ]);
 
-    return responses;
+    // return responses;
+
+    return {};
 };
 
 export default getDecree;
