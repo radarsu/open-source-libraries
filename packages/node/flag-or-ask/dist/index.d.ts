8ab376505e8109bd2b558129683ee43777b4a215
Artur Kurowski <radarsu@gmail.com>
2021-12-10 07:38:14 +0100

    chore(core): update

--- packages/node/flag-or-ask/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import * as inquirer from 'inquirer';
-export declare const getFromFlagOrAsk: (flags: Record<string, string | undefined>, names: string[], type: inquirer.QuestionTypeName) => Promise<any>;
