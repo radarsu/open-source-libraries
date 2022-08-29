0a201057c2506f30bc442a535cb10e7aefd6ed2a
Artur Kurowski <radarsu@gmail.com>
2022-08-29 16:53:25 +0200

    feat(core): switch to pnpm

--- rr-scripts.ts
+++ rr-scripts.ts
@@ -3,13 +3,19 @@ import type { Scripts } from '@radrat/cli';
 const scripts: Scripts = async (cli) => {
     await cli.run({
         name: `install`,
-        command: `npx lerna bootstrap --ignore=rpc-websocket-client`,
+        command: `pnpm install --recursive --filter="\\!rpc-websocket-client"`,
         ignorePackageJsonName: true,
     });
 
     await cli.run({
         name: `build`,
-        command: `npx rr all.run --command=build`,
+        command: `pnpm exec rr all.run --command=build`,
+        ignorePackageJsonName: true,
+    });
+
+    await cli.run({
+        name: `clean`,
+        command: `find . -name "node_modules" -exec rm -rf '{}' +; find . -name "package-lock.json" -exec rm -rf '{}' +;`,
         ignorePackageJsonName: true,
     });
 
