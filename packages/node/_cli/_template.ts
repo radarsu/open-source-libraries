4af40eaea2d53e9f3dc742d619a2a7fcde9e6654
Artur Kurowski <radarsu@gmail.com>
2023-03-27 20:25:30 +0200

    feat: work in progress ts-import@5, templord

--- packages/node/_cli/_template.ts
+++
@@ -1,7 +0,0 @@
-// import type { Template } from 'templord';
-
-const template: any = {
-    patternsToRender: [`package.json`],
-};
-
-export default template;
--- packages/node/_cli/src/commands/generate.spec.ts
+++
@@ -1,3 +0,0 @@
-import * as childProcess from 'child_process';
-
-childProcess.execSync(`./bin/dev generate --from=https://gitlab.radratteam.pl/radioactive-rat/packages/radrat-templates`);
