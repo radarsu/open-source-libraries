661fcc8d9f3c26ec186a02b002d61479ba5b5d11
Artur Kurowski <radarsu@gmail.com>
2025-02-19 12:44:19 +0100

    fix(ts-import): windows compatiblity

--- packages/node/ts-import/src/main.ts
+++ packages/node/ts-import/src/main.ts
@@ -1,6 +1,7 @@
 import * as commentParser from './modules/comment-parser/index.js';
 import * as crossPlatform from './modules/cross-platform/index.js';
 import * as path from 'node:path';
+import * as url from 'node:url';
 import * as utils from './utils/index.js';
 
 import { LoadMode, LoadOptions } from './load.interfaces.js';
@@ -42,7 +43,8 @@ export const load = async (tsRelativePath: string, options?: LoadOptions) => {
 
         // Load from cache.
         if (jsFileExists && !utils.isFileNewer(tsFileExists, jsFileExists)) {
-            const loaded = await import(jsPath);
+            const fileUrl = url.pathToFileURL(jsPath).href;
+            const loaded = await import(fileUrl);
             return loaded;
         }
     }
@@ -53,7 +55,8 @@ export const load = async (tsRelativePath: string, options?: LoadOptions) => {
         ...config,
     });
 
-    const loaded = await import(jsPath);
+    const fileUrl = url.pathToFileURL(jsPath).href;
+    const loaded = await import(fileUrl);
     return loaded;
 };
 
